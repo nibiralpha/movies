c6ef36332b6375c8174c2410d51e69d5f98714cd
Nibir Alam <nibiralpha@gmail.com>
2026-06-22 19:21:03 +0600

    movie detail typescript added

--- src/app/Api/Movies.ts
+++ src/app/Api/Movies.ts
@@ -1,49 +1,87 @@
 import axios, { AxiosResponse } from "axios";
 import { BASEURL } from "../Constant/Api";
 import { TopUpComingMovies } from "../types/TopUpComingMovies";
-import { apiClient } from "@Api/Client"; 
+import { apiClient } from "@Api/Client";
 
-const getTopUpComingMovies = async (page: number = 1): Promise<AxiosResponse<TopUpComingMovies>> => {
+const getTopUpComingMovies = async (
+  page: number = 1,
+): Promise<AxiosResponse<TopUpComingMovies>> => {
   const response = await apiClient.get<TopUpComingMovies>(
     `${BASEURL}/movie/upcoming?page=${page}`,
   );
   return response;
 };
 
-const getTrendingThisWeek = async (page: number = 1): Promise<AxiosResponse<TopUpComingMovies>> => {
+const getTrendingThisWeek = async (
+  page: number = 1,
+): Promise<AxiosResponse<TopUpComingMovies>> => {
   const response = await apiClient.get<TopUpComingMovies>(
     `${BASEURL}/trending/all/week?page=${page}`,
   );
   return response;
 };
 
-const getNowPlaying = async (page: number = 1): Promise<AxiosResponse<TopUpComingMovies>> => {
+const getNowPlaying = async (
+  page: number = 1,
+): Promise<AxiosResponse<TopUpComingMovies>> => {
   const response = await apiClient.get<TopUpComingMovies>(
     `${BASEURL}/movie/now_playing?page=${page}`,
   );
   return response;
 };
 
-const getPopulerMovies = async (page: number = 1): Promise<AxiosResponse<TopUpComingMovies>> => {
+const getPopulerMovies = async (
+  page: number = 1,
+): Promise<AxiosResponse<TopUpComingMovies>> => {
   const response = await apiClient.get<TopUpComingMovies>(
     `${BASEURL}/movie/popular?page=${page}`,
   );
   return response;
 };
 
-const getPopulerTvShows = async (page: number = 1): Promise<AxiosResponse<TopUpComingMovies>> => {
+const getPopulerTvShows = async (
+  page: number = 1,
+): Promise<AxiosResponse<TopUpComingMovies>> => {
   const response = await apiClient.get<TopUpComingMovies>(
     `${BASEURL}/tv/popular?page=${page}`,
   );
   return response;
 };
 
-const getTopRatedTvShows = async (page: number = 1): Promise<AxiosResponse<TopUpComingMovies>> => {
+const getTopRatedTvShows = async (
+  page: number = 1,
+): Promise<AxiosResponse<TopUpComingMovies>> => {
   const response = await apiClient.get<TopUpComingMovies>(
     `${BASEURL}/tv/top_rated?page=${page}`,
   );
   return response;
 };
 
+const getMovieDetails = async (
+  id: number,
+): Promise<AxiosResponse<TopUpComingMovies>> => {
+  const response = await apiClient.get<TopUpComingMovies>(
+    `${BASEURL}/movie/${id}`,
+  );
+  return response;
+};
 
-export { getTopUpComingMovies, getTrendingThisWeek, getNowPlaying, getPopulerMovies, getPopulerTvShows, getTopRatedTvShows };
+const getTvSeriesDetails = async (
+  id: number,
+): Promise<AxiosResponse<TopUpComingMovies>> => {
+  const response = await apiClient.get<TopUpComingMovies>(
+    `${BASEURL}/tv/${id}`,
+  );
+  return response;
+};
+
+export {
+  getTopUpComingMovies,
+  getTrendingThisWeek,
+  getNowPlaying,
+  getPopulerMovies,
+  getPopulerTvShows,
+  getTopRatedTvShows,
+  getMovieDetails,
+  getTvSeriesDetails
+};
