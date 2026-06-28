681a1a98b5f5ef367d6e8836ec043302ec8101ca
Nibir Alam <nibiralpha@gmail.com>
2026-06-29 04:51:25 +0600

    celebrity detail page MediaComponent design

--- src/app/Components/MediaComponent/CelebrityMediaComponent.tsx
+++ src/app/Components/MediaComponent/CelebrityMediaComponent.tsx
@@ -20,17 +20,17 @@ export default function CelebrityMediaComponent({
 }: Readonly<HeaderComponentProps>) {
   //   const [OfficialVideo, setOfficialVideo] = useState<VideoInterface | null>();
 
-//   useEffect(() => {
-//     const writer =
-//       type === "celebrity"
-//         ? null
-//         : type === "movie"
-//           ? getMembers("Writer", data)
-//           : getTvSeriesMembers("Writer", data);
-//     eslint-disable-next-line react-hooks/set-state-in-effect
-//     const video = getOfficialVideo(videos);
-//     setOfficialVideo(video);
-//   }, [data]);
+  //   useEffect(() => {
+  //     const writer =
+  //       type === "celebrity"
+  //         ? null
+  //         : type === "movie"
+  //           ? getMembers("Writer", data)
+  //           : getTvSeriesMembers("Writer", data);
+  //     eslint-disable-next-line react-hooks/set-state-in-effect
+  //     const video = getOfficialVideo(videos);
+  //     setOfficialVideo(video);
+  //   }, [data]);
 
   return (
     <div className="container">
@@ -38,13 +38,52 @@ export default function CelebrityMediaComponent({
         <div className={styles.left_img}>
           <img
             className={styles.img}
-            width={"270px"}
             src={`${TMDB_IMAGE_BASE}/${data.profile_path}`}
           />
         </div>
+        <div className={styles.right_content}>
+          {/* <div className={styles.bottom}>
+            <div className={styles.right_content_name}>Tom Cruise</div>
+            <div className={styles.point}>Acting</div>
+          </div> */}
+          <div className={styles.right_area}>
+            <div className={styles.right_side_content}>
+              <div className={styles.point}>Born</div>
+              <div className={styles.right_side_detail}>November 11, 1999</div>
+            </div>
+            <div className={styles.right_side_content}>
+              <div className={styles.point}>Age</div>
+              <div className={styles.right_side_detail}>49 years old</div>
+            </div>
+            <div className={styles.right_side_content}>
+              <div className={styles.point}>Birthplace</div>
+              <div className={styles.right_side_detail}>
+                Los Angels, California, USA
+              </div>
+            </div>
+            <div className={styles.right_side_content}>
+              <div className={styles.point}>Nationality</div>
+              <div className={styles.right_side_detail}>American</div>
+            </div>
+            <div className={styles.right_side_content}>
+              <div className={styles.point}>Height</div>
+              <div className={styles.right_side_detail}>1.83 m</div>
+            </div>
+            <div className={styles.right_side_content}>
+              <div className={styles.point}>Also know as</div>
+              <div className={styles.right_side_detail}>Leo DiCaprio</div>
+            </div>
+            <div className={styles.right_side_content}>
+              <div className={styles.point}>Years active</div>
+              <div className={styles.right_side_detail}>1990 - preasent</div>
+            </div>
+          </div>
+        </div>
       </div>
       <div className={styles.detail_area}>
-        <div className={styles.details}>{truncateDetails(data.biography, 300)}</div>
+        <div className={styles.details}>
+          {truncateDetails(data.biography, 300)}
+        </div>
       </div>
     </div>
   );
