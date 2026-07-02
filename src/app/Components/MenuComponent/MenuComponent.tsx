4fdf5ad60fe2d549e6cc2313df394485901c4c2c
Nibir Alam <nibiralpha@gmail.com>
2026-07-02 22:23:07 +0600

    minor change

--- src/app/Components/MenuComponent/MenuComponent.tsx
+++ src/app/Components/MenuComponent/MenuComponent.tsx
@@ -23,18 +23,13 @@ export default function MenuComponent() {
             <img src={"/logo.png"} />
           </div>
         </div>
-        <div className={styles.middle}>
+        {/* <div className={styles.middle}>
           <div className={styles.menus}>
             <div className={styles.menu}>
               <div className={styles.menu_name}>Home</div>
-              <div className={styles.icon}>{/* <img src={} /> */}</div>
-            </div>
-            <div className={styles.menu}>
-              <div className={styles.menu_name}>Top 250</div>
-              <div className={styles.icon}>{/* <img src={} /> */}</div>
             </div>
           </div>
-        </div>
+        </div> */}
         <div className={styles.right}>
           <div className={styles.search}>
             <div className={styles.icon}>
